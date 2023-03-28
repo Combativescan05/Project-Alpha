fac741c6ab6b51fbabce1740da5ea0955c94e3f2
Combativescan05 <allmightycheeseboi@gmail.com>
2023-03-28 14:16:44 -0600

    Date 3/28/23 2:16 PM MST

---
+++ Source/ProjectAlpha/MyEBBarrel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EBBarrel.h"
+#include "MyEBBarrel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTALPHA_API UMyEBBarrel : public UEBBarrel
+{
+	GENERATED_BODY()
+	
+};
